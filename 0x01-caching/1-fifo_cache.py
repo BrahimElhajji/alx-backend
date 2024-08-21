2a93ac8098d568b0834791a4f477f9adefde3e31
BrahimElhajji <elhajji080898@gmail.com>
2024-08-21 11:43:47 -0700

    FIFO CACHE

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""a class FIFOCache that inherits from BaseCaching
+and is a caching system"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a FIFO caching system """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+
+            oldest_key = self.order.pop(0)
+            del self.cache_data[oldest_key]
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+
+
+if __name__ == "__main__":
+    my_cache = FIFOCache()
