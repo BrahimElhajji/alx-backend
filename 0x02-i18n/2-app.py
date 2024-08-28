8d0afad155aa03c9138ad449a458b6ff483b1035
BrahimElhajji <elhajji080898@gmail.com>
2024-08-28 02:47:32 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
 This module contains a Flask application with Babel for internationalization.
+It includes a locale selector to determine the best language match.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -23,12 +24,20 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for the supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """
     The index route serves the home page with a simple welcome message.
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
